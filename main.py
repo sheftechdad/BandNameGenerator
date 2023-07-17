692797256ffe98269f9198914b8239e49dffbae8
sheftechdad <shefalichaurasia30945@gmail.com>
2023-07-17 21:39:34 +0530

    My first project

---
+++ main.py
@@ -0,0 +1,4 @@
+print("hello sweety/n")
+place = input("In which city you are grow up\n")
+pet = input("share with me what is your pet  name\n")
+print("where you live and your pet name "+ place + pet)
